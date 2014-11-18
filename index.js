56c817539376d8ec0dfd14371b851255dc1143c9
Kirill Izotov <enykeev@stackstorm.com>
2014-11-18 15:27:52 +0600

    Add more endpoints

--- index.js
+++ index.js
@@ -44,10 +44,14 @@ var request = function (params, body, callback) {
   return req;
 };
 
-var Readable = {
-  // Methods
+var Enumerable = {
   list: {
-    value: function (opts) {
+    value: function (query) {
+      return this.listAll(query);
+    }
+  },
+  listAll: {
+    value: function (query) {
       return new Promise(function (resolve, reject) {
 
         return request({
@@ -56,7 +60,71 @@ var Readable = {
           host: this.host,
           port: this.port,
           path: this.path,
-          query: opts,
+          query: query,
+          withCredentials: false
+        }, function (res) {
+
+          if (res.headers['content-type'].indexOf('application/json') !== -1) {
+            try {
+              res.body = JSON.parse(res.body);
+            } catch (e) {
+              reject(e);
+            }
+          }
+
+          if (res.statusCode >= 400) {
+            return reject(new Error({
+              status: res.statusCode,
+              message: res.body
+            }));
+          }
+
+          // TODO: Enumerable likely should not bother itself with limits or length
+          this.total = +res.headers['x-total-count'] || res.body.length;
+          this.limit = +res.headers['x-limit'] || query && query.limit;
+
+          resolve(res.body);
+
+        }.bind(this)).on('error', function (err) {
+          return reject(err);
+        });
+
+      }.bind(this));
+    }
+  }
+};
+
+var Paginatable = {
+  limit: {
+    value: 10,
+    writable: true
+  },
+
+  list: {
+    value: function (query) {
+      var page = query.page;
+
+      query = assign({}, query);
+      delete query.page;
+
+      return this.listPage(page, query);
+    }
+  },
+
+  listPage: {
+    value: function (page, query) {
+      return new Promise(function (resolve, reject) {
+
+        return request({
+          method: 'get',
+          protocol: this.protocol + ':',
+          host: this.host,
+          port: this.port,
+          path: this.path,
+          query: assign({}, query, {
+            limit: this.limit,
+            offset: (page > 1 ? page - 1 : 0) * this.limit
+          }),
           withCredentials: false
         }, function (res) {
 
@@ -76,7 +144,7 @@ var Readable = {
           }
 
           this.total = +res.headers['x-total-count'] || res.body.length;
-          this.limit = +res.headers['x-limit'] || opts && opts.limit;
+          this.limit = +res.headers['x-limit'] || query && query.limit;
 
           resolve(res.body);
 
@@ -86,8 +154,10 @@ var Readable = {
 
       }.bind(this));
     }
-  },
+  }
+};
 
+var Readable = {
   get: {
     value: function (id) {
       return new Promise(function (resolve, reject) {
@@ -124,6 +194,34 @@ var Readable = {
         });
 
       }.bind(this));
+    }
+  },
+
+  watch: {
+    value: function (id, condition) {
+      var RETRIES = 10
+        , TIMEOUT = 1000;
+
+      var retry = function (retries) {
+        return new Promise(function (resolve, reject) {
+
+          setTimeout(function () {
+
+            this.get(id).then(function (execution) {
+
+              if (retries && !condition(execution)) {
+                return retry(--retries);
+              }
+
+              return execution;
+            }).then(resolve).catch(reject);
+
+          }.bind(this), TIMEOUT);
+
+        }.bind(this));
+      }.bind(this);
+
+      return retry(RETRIES);
     }
   }
 };
@@ -167,34 +265,6 @@ var Writable = {
 
       }.bind(this));
     }
-  },
-
-  watch: {
-    value: function (id, condition) {
-      var RETRIES = 10
-        , TIMEOUT = 1000;
-
-      var retry = function (retries) {
-        return new Promise(function (resolve, reject) {
-
-          setTimeout(function () {
-
-            this.get(id).then(function (execution) {
-
-              if (retries && !condition(execution)) {
-                return retry(--retries);
-              }
-
-              return execution;
-            }).then(resolve).catch(reject);
-
-          }.bind(this), TIMEOUT);
-
-        }.bind(this));
-      }.bind(this);
-
-      return retry(RETRIES);
-    }
   }
 };
 
@@ -216,14 +286,34 @@ module.exports = function (opts) {
   return {
     actions: Object.create(null, assign({
       path: { value: '/actions' }
-    }, Opts, Readable)),
+    }, Opts, Readable, Enumerable)),
 
     actionOverview: Object.create(null, assign({
       path: { value: '/actions/views/overview' }
+    }, Opts, Readable, Enumerable)),
+
+    actionEntryPoint: Object.create(null, assign({
+      path: { value: '/actions/views/entry_point' }
     }, Opts, Readable)),
 
     actionExecutions: Object.create(null, assign({
       path: { value: '/actionexecutions' }
-    }, Opts, Readable, Writable))
+    }, Opts, Readable, Writable, Enumerable)),
+
+    history: Object.create(null, assign({
+      path: { value: '/history/executions' }
+    }, Opts, Readable, Paginatable)),
+
+    historyFilters: Object.create(null, assign({
+      path: { value: '/history/executions/views/filters' }
+    }, Opts, Enumerable)),
+
+    rules: Object.create(null, assign({
+      path: { value: '/rules' }
+    }, Opts, Readable, Writable, Enumerable)),
+
+    triggerTypes: Object.create(null, assign({
+      path: { value: '/triggertypes' }
+    }, Opts, Readable, Enumerable))
   };
 };
