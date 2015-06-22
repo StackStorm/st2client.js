35685fdfb64ae832764411cb05e02d9ada066598
Kirill Izotov <enykeev@stackstorm.com>
2015-06-22 23:00:50 +0600

    Add attributable mixin

---
+++ tests/test-attributable.js
@@ -0,0 +1,79 @@
+/*global describe, it*/
+'use strict';
+
+var chai = require('chai')
+  , chaiAsPromised = require("chai-as-promised")
+  , endpoint = require('../lib/endpoint')
+  , nock = require('nock')
+  , rsvp = require('rsvp')
+  , Opts = require('./opts')
+  ;
+
+chai.use(chaiAsPromised);
+nock.disableNetConnect();
+
+var all = rsvp.all
+  , expect = chai.expect
+  , Attributable = require('../lib/mixins/attributable')
+  , mock = nock('http://localhost')
+  ;
+
+describe('Attributable', function () {
+
+  var api = endpoint('/test', Opts, Attributable);
+
+  describe('#attribute()', function () {
+
+    it('should return a promise of a single entity', function () {
+      var response = {};
+
+      mock.get('/v1/test/1/attribute/some')
+        .reply(200, response);
+
+      var result = api.attribute(1, 'some');
+
+      return all([
+        expect(result).to.eventually.be.an('object'),
+        expect(result).to.eventually.be.deep.equal(response)
+      ]);
+    });
+
+    it('should throw an error if no id is provided', function () {
+      var fn = function () {
+        api.attribute();
+      };
+
+      expect(fn).to.throw('is not a valid id');
+    });
+
+    it('should throw an error if no attribute is provided', function () {
+      var fn = function () {
+        api.attribute(1);
+      };
+
+      expect(fn).to.throw('is not a valid attribute name');
+    });
+
+    it('should reject the promise if server returns 4xx or 5xx status code', function () {
+      var response = {
+        faultstring: 'errortext'
+      };
+
+      mock.get('/v1/test/1/attribute/some')
+        .reply(400, response);
+
+      var result = api.attribute(1, 'some');
+
+      return all([
+        expect(result).to.be.rejected,
+        result.catch(function (err) {
+          expect(err).to.have.property('name', 'APIError');
+          expect(err).to.have.property('status', 400);
+          expect(err).to.have.property('message', 'errortext');
+        })
+      ]);
+    });
+
+  });
+
+});
