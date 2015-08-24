2a4b21ff38b7d69a81a5d18fa41d2ea92120e342
Edward Medvedev <edward.medvedev@gmail.com>
2015-08-24 19:44:26 +0600

    Routable tests

---
+++ tests/test-routable.js
@@ -0,0 +1,38 @@
+/*global describe, it*/
+'use strict';
+
+var chai = require('chai')
+  , chaiAsPromised = require("chai-as-promised")
+  , endpoint = require('../lib/endpoint')
+  , Opts = require('./opts')
+  ;
+
+chai.use(chaiAsPromised);
+
+var expect = chai.expect
+  , Routable = require('../lib/mixins/routable')
+  ;
+
+describe('Routable', function () {
+
+  var api = endpoint('/test', Opts, Routable);
+
+  describe('#route()', function () {
+
+    it('should return a route of an API method', function () {
+      var url = '/v1/test/1';
+      var result = api.route(1);
+
+      return expect(result).to.be.equal(url);
+    });
+
+    it('should return a route of an API method with a query', function () {
+      var url = '/v1/test/1?a=b';
+      var result = api.route(1, {a: 'b'});
+
+      return expect(result).to.be.equal(url);
+    });
+
+  });
+
+});
