6e06274a17bc684aca8d88a8539a73762c80dec3
blag <blag@users.noreply.github.com>
2019-07-25 02:32:37 -0700

    Add testcase for Schedulable mixin

---
+++ tests/test-schedulable.js
@@ -0,0 +1,67 @@
+/*global describe, it*/
+'use strict';
+
+var chai = require('chai')
+  , chaiAsPromised = require("chai-as-promised")
+  , endpoint = require('../lib/endpoint')
+  , nock = require('nock')
+  , Opts = require('./opts')
+  ;
+
+chai.use(chaiAsPromised);
+nock.disableNetConnect();
+
+var expect = chai.expect
+  , Schedulable = require('../lib/mixins/schedulable')
+  , mock = nock('http://localhost', {
+    reqheaders: {
+      'content-type': 'application/json;charset=utf-8'
+    }
+  })
+  ;
+
+describe('Schedulable', function () {
+
+  var api = endpoint('/test', Opts, Schedulable).value;
+
+  describe('#schedule()', function () {
+
+    it('should return a promise of a single entity', function () {
+      var request = {}
+        , response = {}
+        ;
+
+      mock.post('/v1/test', request)
+        .reply(202, response);
+
+      var result = api.schedule({}, 'test');
+
+      return Promise.all([
+        expect(result).to.eventually.be.an('object'),
+        expect(result).to.eventually.be.deep.equal(response)
+      ]);
+    });
+
+    it('should reject the promise if server returns other than 202 status code', function () {
+      var request = {}
+        , response = 'accepted instead of scheduled'
+        ;
+
+      mock.post('/v1/test', request)
+        .reply(201, response);
+
+      var result = api.schedule({}, 'test');
+
+      return Promise.all([
+        expect(result).to.be.rejected,
+        result.catch(function (err) {
+          expect(err).to.have.property('name', 'APIError');
+          expect(err).to.have.property('status', 201);
+          expect(err).to.have.property('message', response);
+        })
+      ]);
+    });
+
+  });
+
+});
